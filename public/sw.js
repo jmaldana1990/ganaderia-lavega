d4e0b034a90f699516745e55d67471afdc582ba8
jmaldana1990 <jmaldana1990@gmail.com>
2026-02-17 17:34:47 -0500

    Delete public/sw.js

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-// Unregister and clean all caches
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-      .then(() => self.registration.unregister())
-  );
-});
